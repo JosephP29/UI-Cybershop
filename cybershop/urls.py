1feba4200f8601c2f406c4d9ddcc36fb5cbc585b
Joseph Price <jwp229@gmail.com>
2018-03-21 17:31:21 -0500

    shop page that displays add produts

--- cybershop/urls.py
+++ cybershop/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
-    url('admin/', admin.site.urls)
+    url('admin/', admin.site.urls),
+    url(r'^shop/', include('shop.urls')),
     #url('login/', view.login)
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from shop.models import Product
 
 # Create your views here.
+def index(request):
+	productList = Product.objects.all()
+	args = { 'products': productList }
+	return render(request, 'shop/index.html', args)
